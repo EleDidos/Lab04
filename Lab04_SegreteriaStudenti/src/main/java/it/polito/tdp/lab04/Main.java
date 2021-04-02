2c6e3987f5abff247d26d74665423b4232bd9789
elena <elena@localhost>
2021-04-02 23:27:58 +0200

    definitivo 1.0

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/Main.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/Main.java
@@ -1,5 +1,13 @@
 package it.polito.tdp.lab04;
 
+/********** DA FARE ***************
+ * 1. ISCRIZIONE nel DAO
+ * 2. non appaiono le righe
+ * 
+ * @author elena
+ *
+ */
+
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
